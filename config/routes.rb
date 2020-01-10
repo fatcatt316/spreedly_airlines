22de530c281bd5377d30a7de80239eaf831a4e3c
Joe Peck <joe@spreedly.com>
2020-01-10 16:35:14 -0500

    Add Transactions controller and a route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flights, only: :index
+  resources :transactions, only: :index
+
+  get "🛩", to: 'flights#index'
 
   root 'flights#index'
 end
