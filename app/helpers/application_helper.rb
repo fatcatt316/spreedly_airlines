19c5bb2e3656f570ad4e9ae6edb1466825ee1fe8
Joe Peck <joe@spreedly.com>
2020-01-15 15:54:38 -0500

    Improve navigation links a tiny bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flights_navigation_link
+    return 'Flights' if controller_name == 'flights' && action_name == 'index'
+    link_to('Flights', flights_path)
+  end
+
+  def transactions_navigation_link
+    return 'Transactions' if controller_name == 'transactions' && action_name == 'index'
+    link_to('Transactions', transactions_path)
+  end
 end
